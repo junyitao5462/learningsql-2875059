3a16795b089b1300b2df0b1222be398d09d70a48
junyitao5462 <81429285+junyitao5462@users.noreply.github.com>
2025-05-29 15:59:20 -0400

    added compound select

--- SQLite.sql
+++
@@ -1,5 +0,0 @@
--- SQLite
-SELECT first_name, COUNT (first_name)
-FROM people
-GROUP BY first_name
-ORDER BY COUNT (first_name) DESC;
--- wroking.sql
+++ wroking.sql
@@ -12,7 +12,20 @@ FROM people
 GROUP BY first_name
 ORDER BY COUNT (first_name) DESC;
 
+--compound select
+SELECT first_name, last_name, quiz_points 
+FROM people 
+WHERE quiz_points=(SELECT MAX(quiz_points) FROM people);
 
-
-
+-- alias
+SELECT first_name as firstname, UPPER(last_name) as surname 
+FROM people
+WHERE first_name ='Wanda';
  
+ -- quiz
+ SELECT state_name AS STATE, MAX (quiz_points) AS MAXPOINTS, AVG (quiz_points) AS
+ AVGPOINTS
+ FROM people
+ JOIN states ON states.state_abbrev = people.state_code
+ GROUP BY state_code
+ ORDER BY AVGPOINTS DESC;     --can use alias here
